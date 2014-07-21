7371259e729450969b0e616c87889250040423c9
Abaho Katabarwa <abkataba@cisco.com>
2014-07-21 08:14:41 -0700

    buttons in ui almost working

--- save-the-trees/app/scripts/app.js
+++ save-the-trees/app/scripts/app.js
@@ -7,10 +7,21 @@
  *
  * Main module of the application.
  */
-angular
-  .module('EcoCisco', [])
+(function() {
+  var app = angular.module('EcoCisco', []); 
 
-  .config(function ($routeProvider) {
+  app.controller("GlobalController", function($scope) {
+    $scope.state = {
+      isLoggedIn: false
+    };
+
+    $scope.changeLoginState = function() {
+      this.state.isLoggedIn = true;
+    };
+
+  });
+
+  app.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -23,4 +34,62 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  
+  app.controller('LoginController', function($scope) {
+
+      $scope.credentials = {
+        username: '',
+        password: ''
+      };
+
+      $scope.login = function() {
+        this.changeLoginState();
+      };
+
   });
+
+  app.controller('SideNavController', function($scope) {
+
+    $scope.buttons = [{
+      label: "My EcoCisco", 
+      class: "fa fa-dashboard",
+      selected: true
+    }, {
+      label: "My EcoStanding",
+      class: "fa fa-table",
+      selected: false
+    }, {
+      label: "My EcoEnergy",
+      class: "fa fa-bar-chart-o",
+      selected: false
+    }, {
+      label: "About",
+      class: "fa fa-edit",
+      selected: false
+    }];
+
+    function returnSelectedButton(array) {
+      array.forEach(function(element) {
+        if (element.selected) {
+          return element;
+        }
+      });
+      return null;
+    }
+
+    $scope.isSelected = function(index) {
+      return (this.buttons[index].selected);
+    }
+
+    $scope.toggle = function(index) {
+      var prevSelectedElem = returnSelectedButton(this.buttons);
+      if (prevSelectedElem) {
+        prevSelectedElem.selected = false;
+      }
+      this.buttons[index].selected = true;
+    }
+
+  });
+
+})();
